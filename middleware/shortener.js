af1f70962faf9da8ac124ed0bfc9ab95a902e352
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-22 21:59:42 +0600

    Re-enable MongoDB URL shortener insertion

--- middleware/shortener.js
+++ middleware/shortener.js
@@ -4,10 +4,10 @@ const Shortener = require("../model/shortener.model");
 const compressUrl = async (originalUrl) => {
   try {
     const shortId = Math.random().toString(36).substring(2, 7);
-    // const shortener = await Shortener.collection.insertOne({ originalUrl, shortUrl: shortId });
-    // if (!shortener) {
-    //   throw new Error("Failed to compress URL");
-    // }
+    const shortener = await Shortener.collection.insertOne({ originalUrl, shortUrl: shortId });
+    if (!shortener) {
+      throw new Error("Failed to compress URL");
+    }
 
     const shortUrlFull = `${process.env.BASE_URL}/${shortId}`;
     return shortUrlFull;
