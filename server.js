84159a1ac9484cfe5056d1807f0347b6e7f7c326
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-22 19:09:32 +0600

    config router

--- server.js
+++ server.js
@@ -3,34 +3,50 @@ const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require("path");
+const router = require("./router/index");
+const { connectDB } = require("./configDB/connectDB");
+const { compressUrl } = require("./middleware/shortener");
+require("dotenv").config();
 app.use(bodyParser.json());
 
 app.get("/health", (req, res) => {
   res.send("Server is Live and Running");
 });
 
-app.get("/", async(req, res, next) => {
-    try {
-        const filePath = path.join(__dirname, './view/index.ejs');
-         ejs.renderFile(filePath, {shortUrl: 'https://short.url/1234567890', username: 'John Doe'}, (err, str) => {
-            if (err) {
-                return res.status(500).send('Error rendering template');
-            }
-          
-         return  res.send(str);
-        })
-    } catch (error) {
-        next(error);
-    }
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+
+app.get("/", (req, res) => {
+  res.render("index", { shortUrl: null, originalUrl: "" });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const { originalUrl } = req.body;
+    console.log("User Input:", originalUrl);
+    const shortUrl = await compressUrl(originalUrl);
+    res.render("index", { shortUrl, originalUrl });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+app.use("/", router);
+
+app.get("/test", async (req, res) => {
+  const shortUrl = await compressUrl("https://www.google.com");
+  res.send(shortUrl);
 });
 
 app.use((error, req, res, next) => {
-    const message = error.message ? error.message : "Server Error Occured";
-    const status = error.status ? error.status : 500;
-    res.status(status).json({ success: false, message });
-  });
+  const message = error.message ? error.message : "Server Error Occured";
+  const status = error.status ? error.status : 500;
+  res.status(status).json({ success: false, message });
+});
 
 const port = 5003;
 app.listen(port, async () => {
   console.log(`server running on http://localhost:${port}`);
+  await connectDB();
 });
