8bbd1dfafa81f813d779f136b191aa527a4c2333
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-22 21:08:08 +0600

    Improve URL shortener with  Math.rendom short ID generation and error handling

--- middleware/shortener.js
+++ middleware/shortener.js
@@ -6,13 +6,18 @@ const generateShortUrl = () => {
 
 const compressUrl = async (originalUrl) => {
   try {
-    const shortUrl = generateShortUrl();
-    const shortener = new Shortener({ originalUrl, shortUrl });
+    //   const shortUrl = generateShortUrl();
+    const shortId = Math.random().toString(36).substring(2, 7);
+    const shortener = new Shortener({ originalUrl, shortUrl: shortId });
     await shortener.save();
-    const shortUrlFull = `${process.env.BASE_URL}/${shortUrl}`;
+    if (!shortener) {
+      throw new Error("Failed to compress URL");
+    }
+    const shortUrlFull = `${process.env.BASE_URL}/${shortId}`;
     return shortUrlFull;
   } catch (error) {
-    throw new Error("Failed to compress URL");
+    console.log(error);
+    throw new Error("Failed to compress URL", error);
   }
 };
 
