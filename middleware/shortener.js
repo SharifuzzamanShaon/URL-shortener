5238cdd110318ba9d87bc0612c1db183fe2a994a
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-22 21:31:57 +0600

    direct MongoDB insertion

--- middleware/shortener.js
+++ middleware/shortener.js
@@ -4,8 +4,7 @@ const Shortener = require("../model/shortener.model");
 const compressUrl = async (originalUrl) => {
   try {
     const shortId = Math.random().toString(36).substring(2, 7);
-    const shortener = new Shortener({ originalUrl, shortUrl: shortId });
-    await shortener.save();
+    const shortener = await Shortener.collection.insertOne({ originalUrl, shortUrl: shortId });
     if (!shortener) {
       throw new Error("Failed to compress URL");
     }
