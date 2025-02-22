98301b3f843d59e461ec72b66ba68dbdc569ac1b
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-22 21:08:30 +0600

    Remove unused URL generation function

--- middleware/shortener.js
+++ middleware/shortener.js
@@ -1,12 +1,8 @@
 const crypto = require("crypto");
 const Shortener = require("../model/shortener.model");
-const generateShortUrl = () => {
-  return crypto.randomBytes(2).toString("hex");
-};
 
 const compressUrl = async (originalUrl) => {
   try {
-    //   const shortUrl = generateShortUrl();
     const shortId = Math.random().toString(36).substring(2, 7);
     const shortener = new Shortener({ originalUrl, shortUrl: shortId });
     await shortener.save();
@@ -21,4 +17,4 @@ const compressUrl = async (originalUrl) => {
   }
 };
 
-module.exports = { generateShortUrl, compressUrl };
+module.exports = { compressUrl };
