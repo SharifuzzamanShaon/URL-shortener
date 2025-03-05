c993302bb28165417b87ce0f5660ba87d78aa392
Shuvo Prosad Sarnakar <shuvoprosad@gmail.com>
2025-03-05 23:00:12 +0600

    Update redirect logic to construct full URL for short URL lookup

--- controller/redirectToOrginal.js
+++ controller/redirectToOrginal.js
@@ -3,7 +3,8 @@ const Shortener = require("../model/shortener.model");
 const redirectToOrginal = async (req, res) => {
   try {
     const { shortUrl } = req.params;
-    const shortener = await Shortener.findOne({ shortUrl });
+    const getUrlFor = `${process.env.BASE_URL}/${shortUrl}`;
+    const shortener = await Shortener.findOne({ shortUrl: `${getUrlFor}` });
     if (!shortener) {
       return res.status(404).json({ message: "Short URL not found" });
     }
